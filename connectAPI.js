eabc5c2c2e04e8919090a7b892582d1bb6ed872b
Ahmed Nail Al Drabea <124718632+ahmedaldrabea@users.noreply.github.com>
2024-07-11 21:26:59 +0300

    Update connectAPI.js

--- connectAPI.js
+++ connectAPI.js
@@ -1,76 +1,51 @@
 function getPost(neededPost){
-    let request = new XMLHttpRequest();
+    fetch("https://jsonplaceholder.typicode.com/posts?userId=" + neededPost)
+    .then((responseData) => {
+        if(responseData.ok)
+            return responseData.json()
+        else
+            alert("Server Error!")
     
-    request.open("GET","https://jsonplaceholder.typicode.com/posts?userId=" + neededPost )
-    request.responseType = 'json'; /* Important Section */
-    
-    request.setRequestHeader("Accept","application/json")
-    request.setRequestHeader("Content-Type","application/json")
+    }).then(jsonPost => {
 
-    request.send();
-
-    request.onload = function(){
-        if(request.status >= 200 && request.status < 300 ){
-            let responseProcess = request.response;
-
-            let postSection = document.getElementById('posts');
+        let postSection = document.getElementById('posts');
             postSection.innerHTML = '';
 
-            for(response of responseProcess){
-                let content = 
-                `
-                    <div  class="post">
-                        <h3>${response.title}</h3>
-                        <p>${response.body}</p>
-                    </div>
-                `
-                postSection.innerHTML += content;
-            }
-
-        }else{
-            alert("Server Error!");
+        for(response of jsonPost){
+            let content = 
+            `
+                <div class="post">
+                    <h3>${response.title}</h3>
+                    <p>  ${response.body}</p>
+                </div>
+            `
+            postSection.innerHTML += content;
         }
-    }
+    });
 }
 
 function getUsers(){
-    let request = new XMLHttpRequest();
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((responseData) => {
+        if(responseData.ok)/* This case about if you have an error in status >= 200 and < 300 */
+            return responseData.json() /* Will return a promises to be implemented in then ! */
+        else
+            alert("Server Error!")
     
-    request.open("GET","https://jsonplaceholder.typicode.com/users")
-    request.responseType = 'json'; /* Important Section */
-    
-    request.setRequestHeader("Accept","application/json")
-    request.setRequestHeader("Content-Type","application/json")
-
-    request.send();
-
-    request.onload = function(){
-        if(request.status >= 200 && request.status < 300 ){
-            let usersProcess = request.response;
-
-  
-            let users = document.getElementById('user-data2');
-            users.innerHTML = " ";
-
-            for(userData of usersProcess){
-                let content = 
-                `
-                     <div onclick="showUserInfo(${userData.id})" id="user-section" class="user">
-                        <h3> <span> <i class="fa-solid fa-user-tie"></i> </span> ${userData.name} </h3>
-                        <p>  <span> <i class="fa-solid fa-envelope"></i> </span> ${userData.email}  </p>
-                    </div>
-                `
-                users.innerHTML += content;
-            }
-
-        }else{
-            alert("Server Error!");
+    }).then(json => {
+        let users = document.getElementById('user-data2');
+        users.innerHTML = " ";
+        
+        for(userData of json){
+            let content =
+            `
+                <div onclick="getPost(${userData.id})" id="user-section" class="user">
+                    <h3> <span> <i class="fa-solid fa-user-tie"></i> </span> ${userData.name} </h3>
+                    <p>  <span> <i class="fa-solid fa-envelope"></i> </span> ${userData.email}  </p>
+                </div>
+            `
+            users.innerHTML +=content;
         }
-    }
-}
-
-getUsers()
-
-function showUserInfo(id){
-    getPost(id);
-}
+    });
+}getUsers()
